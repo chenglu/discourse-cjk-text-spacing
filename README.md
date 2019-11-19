c2051d83e1b29864c6da2c55c825a6f74a240c37
marguerite <i@marguerite.su>
2019-11-19 14:47:33 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+discourse-cjk-text-spacing
+
+------
+
+A discourse plugin to automatically add reasonable half-width spaces between Latin and CJK characters
+in topic title and post content.
+
---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# name: discourse-cjk-text-spacing
+# about: automatically add a half-width whitespace between Latin/CJK characters.
+# version: 1.0.0
+# authors: Marguerite Su <marguerite@opensuse.org>
+# url: https://github.com/openSUSE-zh/discourse-cjk-text-spacing
+
+gem 'auto-correct', '0.1.0.pre0'
+
+
