4059f82c9e3ca86e869bdd43ebaef7fb8acac5b0
Luke Cheng <chenglu@users.noreply.github.com>
2020-10-25 23:54:24 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -47,8 +47,9 @@ after_initialize do
       @user = user
       args[:title] = args[:title].auto_correct! if args[:title].present?
       args[:raw] = args[:raw].auto_correct!
-      args[:raw] = args[:raw].gsub '** ', '**'
-      args[:raw] = args[:raw].gsub ' **', '**'
+      if string.match(/(.*)\*\s(.*)\s\*/m)
+        args[:raw] = args[:raw].gsub('* ', '*').gsub(' *', '*')
+      end
       @args = args.delete_if { |_, v| v.nil? }
     end
   end
@@ -58,7 +59,9 @@ after_initialize do
 
     def revise!(editor, fields, opts = {})
       fields[:raw] = fields[:raw].auto_correct! if fields[:raw].present?
-      fields[:raw] = fields[:raw].gsub('** ', '**').gsub(' **', '**') if fields[:raw].present?
+      if string.match(/(.*)\*\s(.*)\s\*/m)
+        fields[:raw] = fields[:raw].gsub('* ', '*').gsub(' *', '*') if fields[:raw].present?
+      end
       fields[:title] = fields[:title].auto_correct! if fields[:title].present?
       old_revise!(editor, fields, opts)
     end
